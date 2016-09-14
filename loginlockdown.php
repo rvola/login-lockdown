649c7317c337e1f4a166d21e671184060fa99276
Nicolas GEHIN <nicolas@rvola.com>
2016-09-14 13:10:05 +0200

    MINOR / update license

--- loginlockdown.php
+++ loginlockdown.php
@@ -10,35 +10,22 @@ Domain Path: /languages/
 Text Domain: loginlockdown
 */
 
-/*
-/--------------------------------------------------------------------\
-|                                                                    |
-| License: GPL                                                       |
-|                                                                    |
-| Login LockDown - added security measures to WordPress intended to  |
-| inhibit or reduce brute force password discovery.                  |
-| Copyright (C) 2007 - 2014, Michael VanDeMar,                              |
-| http://www.bad-neighborhood.com                                    |
-| All rights reserved.                                               |
-|                                                                    |
-| This program is free software; you can redistribute it and/or      |
-| modify it under the terms of the GNU General Public License        |
-| as published by the Free Software Foundation; either version 2     |
-| of the License, or (at your option) any later version.             |
-|                                                                    |
-| This program is distributed in the hope that it will be useful,    |
-| but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-| GNU General Public License for more details.                       |
-|                                                                    |
-| You should have received a copy of the GNU General Public License  |
-| along with this program; if not, write to the                      |
-| Free Software Foundation, Inc.                                     |
-| 51 Franklin Street, Fifth Floor                                    |
-| Boston, MA  02110-1301, USA                                        |   
-|                                                                    |
-\--------------------------------------------------------------------/
-*/
+/**
+ *
+ *	This program is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU General Public License
+ *	as published by the Free Software Foundation; either version 2
+ *	of the License, or (at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 $loginlockdown_db_version = "1.0";
 $loginlockdownOptions = get_loginlockdownOptions();
